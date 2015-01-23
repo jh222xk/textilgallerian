d36314c80c8ebd52f2bccda18a5d311fa051b65e
Grenmyr <dg222cs@student.lnu.se>
2015-01-23 14:37:39 +0100

    feat(Domain)

---
+++ TextilgallerianKuponger/Domain/Entities/BuyXProductsPayForYProducts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Discount: Customer only pays for Y Products when buying X products from BuyProducts-list
+    /// </summary>
+    public class BuyXProductsPayForYProducts : Coupon
+    {
+        // List amount of Products needed to buy. 
+        public List<Product> BuyProducts { get; set; }
+
+        // How many customer need to buy
+        public int Buy { get; set; }
+
+        // How many free
+        public int PayFor { get; set; }
+    }
+}
---
+++ TextilgallerianKuponger/Domain/Entities/TotalSumAmountDiscount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Class to give discount in kronor from total sum.
+    /// </summary>
+    public class TotalSumAmountDiscount : Coupon
+    {
+        public Decimal Amount { get; set; }
+    }
+}
