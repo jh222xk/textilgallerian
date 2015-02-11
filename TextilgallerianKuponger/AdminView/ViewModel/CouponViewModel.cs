4a43fa1b4c2dbe99a3fb125d9f430b28b297a5a6
jh222vp <jh222vp@Ny106x08.lnu.se>
2015-02-11 15:43:48 +0100

    Users and coupons views

--- TextilgallerianKuponger/AdminView/ViewModel/CouponViewModel.cs
+++ TextilgallerianKuponger/AdminView/ViewModel/CouponViewModel.cs
@@ -22,6 +22,59 @@ namespace AdminView.ViewModel
         public IEnumerable<User> Users { get; set; }
         public User User { get; set; }
 
+        public List<Coupon> Coupon1
+        {
+            get
+            {
+                return new List<Coupon>{
+                        new BuyProductXRecieveProductY
+                        {
+                            Code = "xmas14",
+                            Start = System.DateTime.Now,
+                            End = System.DateTime.Now.AddDays(1)
+                            
+                        },
+                        new TotalSumAmountDiscount
+                        {
+                            Code = "jul",
+                            Start = System.DateTime.Now,
+                            End = System.DateTime.Now.AddDays(1)                            
+                        },
+                        new TotalSumPercentageDiscount
+                        {
+                            Code = "påsk",
+                            Start = System.DateTime.Now,
+                            End = System.DateTime.Now.AddDays(1)    
+                        }
+                    };
+            }
+        }
+
+
+
+        public List<User> Users1
+        {
+            get
+            {
+                return new List<User>{
+                        new User
+                        {
+                            Username = "Anna Bok",
+                            Email = "Anna.Bok@mail.se",
+                        },
+                        new User
+                        {
+                            Username = "Roger Pontare",
+                            Email = "Roger.P@mail.com"
+                        },
+                        new User
+                        {
+                            Username = "Hannibal",
+                            Email = "Brains@mail.com"
+                        }
+                    };
+            }
+        }
         public IEnumerable<Coupon> Coupons { get; set; }
     }
 }
