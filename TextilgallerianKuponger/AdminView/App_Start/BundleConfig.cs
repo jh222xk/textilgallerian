d0b29fb3fe627723f70df298a966a2ed688c3a9a
jh222xk <jh222xk@student.lnu.se>
2015-02-16 10:19:08 +0100

    fix: Fixed bundle config

--- TextilgallerianKuponger/AdminView/App_Start/BundleConfig.cs
+++ TextilgallerianKuponger/AdminView/App_Start/BundleConfig.cs
@@ -8,24 +8,13 @@ namespace AdminView {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/semantic").Include(
                       "~/Scripts/semantic.min.js",
-                      "~/Scripts/semantic.site.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/semantic.site.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                     "~/Content/jquery-ui.min.css",
-                     "~/Content/jquery.highlighttextarea.min.css",
-                      "~/Content/semantic.min.css",
-                      "~/Content/site.css"));
+                      "~/Content/Css/semantic.min.css",
+                      "~/Content/Css/site.css"));
         }
     }
 }
