0ddb5ae8c0ea5e018e45687556068401e58e32ca
em222iv <erikper.magnusson@gmail.com>
2015-03-02 13:32:31 +0100

    jsonklart

--- TextilgallerianKuponger/AdminView/Scripts/JSON.js
+++ TextilgallerianKuponger/AdminView/Scripts/JSON.js
@@ -1,9 +1,9 @@
 ﻿window.addEventListener('load', function () {
-    var categories;
-    var content="";
+    var json;
+    var content = [];
+   
     (function ($) {
         var url = 'http://eerie.se/textil/kategorier.json?callback=?';
-
         $.ajax({
             type: 'GET',
             url: url,
@@ -11,9 +11,12 @@
             jsonpCallback: 'jsonCallback',
             contentType: "application/json",
             dataType: 'jsonp',
-            success: function (json) {
-                categories = json;
+            success: function (jsoncallback) {
+                console.log(jsoncallback);
+                json = jsoncallback;
                 categoryDropDown();
+                brandDropDown();
+                productSearch();
            },
             error: function (e) {
                 console.log(e.message);
@@ -21,8 +24,8 @@
         });
     })(jQuery);
     function categoryDropDown() {
-        for (var key in categories) {
-            var obj = categories["KATEGORIER"];
+ 
+            var obj = json["KATEGORIER"];
             for (var prop in obj) {
                 var div = document.getElementById("category-dropdown")
                 var opt = document.createElement('option');
@@ -30,25 +33,75 @@
                 opt.innerHTML = prop;
                 div.appendChild(opt);
             }
-        }
-    }
-    $('.dropdown')
+        
+    };
+    function brandDropDown() {
+ 
+            var obj = json["BRANDS"];
+            for (var prop in obj) {
+                var div = document.getElementById("brand-dropdown")
+                var opt = document.createElement('option');
+                opt.value = prop;
+                opt.innerHTML = prop;
+                div.appendChild(opt);
+            }
+        
+    };
+    function productSearch() {
+        jQuery.each(json["BRANDS"], function (brandkey, brandvalue) {
+            jQuery.each(brandvalue["PRODUCTS"], function (productKey, productValue) {
+                
+                content.push({
+                    title: productKey,
+                    description: productValue,
+                    price: json[brandvalue]
+                });
+            }); searchUpdate();
+        }); 
+    };
+    function searchUpdate() {
+        $('.ui.search')
+         .search({
+             onSelect: function (result, response) {
+                 console.log(result["title"]);
+                 content = $('textarea#products').val();
+                 var item = result["description"] + "\n";
+                 content = content + result["description"];
+                 $('textarea#products').val(content + "\n")
+             },
+             source: content,
+             searchFields: [
+               'title', 'description'
+             ],
+             searchFullText: false
+         });
+    };
+
+    $('#category-dropdown')
       .dropdown({
           onChange: function (value, text) {
               content = $('textarea#sub-categories').val();
-              $.each(categories["KATEGORIER"][text], function (i, l) {
-
+              $.each(json["KATEGORIER"][text], function (i, l) {
+                 
                   var item = l+"\n";
                   content = content + item;
              
               });
              
-        
               $('textarea#sub-categories').val(content + "\n")
-              console.log($('textarea#sub-categories').val());
-            
           }
       })
     ;
+    $('#brand-dropdown')
+     .dropdown({
+         onChange: function (value, text) {
+             content = $('textarea#brands').val();
+
+                 var item = value + "\n";
+                 content = content + value;
+             $('textarea#brands').val(content + "\n")
+             console.log($('textarea#brands').val());
+         }
+     });
 });
 
